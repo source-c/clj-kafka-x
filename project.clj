50a3d620b65799facbaa25b4bcc6fc6c0bd9b5f9
MelKori <mieldo@gmail.com>
2021-05-25 22:26:23 +0300

    update native kafka

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/source-c/clj-kafka-x"
   :license {:name "Apache License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.apache.kafka/kafka_2.12 "2.7.0"
+  :dependencies [[org.apache.kafka/kafka_2.12 "2.8.0"
                   :exclusions [javax.jms/jms
                                com.sun.jdmk/jmxtools
                                com.sun.jmx/jmxri
                                org.slf4j/slf4j-log4j12]]
-                 [org.apache.zookeeper/zookeeper "3.6.2"
+                 [org.apache.zookeeper/zookeeper "3.7.0"
                   :exclusions [org.slf4j/slf4j-log4j12]]
-                 [org.apache.kafka/kafka-clients "2.7.0"]])
+                 [org.apache.kafka/kafka-clients "2.8.0"]])
