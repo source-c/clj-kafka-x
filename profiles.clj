04cd6623a2a8ac7f0d2acea17c781d64ebb9fb6d
MelKori <mieldo@gmail.com>
2021-05-25 22:27:15 +0300

    update midje

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
-{:dev      {:dependencies [[midje "1.9.10"]]}
+{:dev      {:dependencies [[midje "1.10.3"]]}
  :docs     {:plugins [[lein-codox "0.10.7"]
                       [org.timmc/nephila "0.3.0"]]}
  :provided {:dependencies [[org.clojure/clojure "1.10.3"]]}
