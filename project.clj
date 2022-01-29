ea3a96a250023a294b58fc1849b1a3c00ace7e5d
MelKori <mieldo@gmail.com>
2022-01-29 20:50:26 +0200

    udpate deps

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/source-c/clj-kafka-x"
   :license {:name "Apache License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.apache.kafka/kafka_2.12 "3.0.0"
+  :dependencies [[org.apache.kafka/kafka_2.12 "3.1.0"
                   :exclusions [javax.jms/jms
                                com.sun.jdmk/jmxtools
                                com.sun.jmx/jmxri
                                org.slf4j/slf4j-log4j12]]
                  [org.apache.zookeeper/zookeeper "3.7.0"
                   :exclusions [org.slf4j/slf4j-log4j12]]
-                 [org.apache.kafka/kafka-clients "3.0.0"]])
+                 [org.apache.kafka/kafka-clients "3.1.0"]])
