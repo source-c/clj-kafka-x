b356dabdde0c16e5f2f672f8dd2bedd0ad603498
MelKori <mieldo@gmail.com>
2022-10-24 12:44:39 +0300

    update kafka to 3.3.1

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
-{:dev      {:dependencies [[midje "1.10.5"]]}
+{:dev      {:dependencies [[midje "1.10.6"]]}
  :docs     {:plugins [[lein-codox "0.10.8"]
                       [org.timmc/nephila "0.3.0"]]}
  :provided {:dependencies [[org.clojure/clojure "1.11.1"]]}
