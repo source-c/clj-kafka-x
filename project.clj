74d567b610481873007cee542a1b9bd5d1d0ea1d
A I <io-devel@copper.mx>
2025-04-13 01:44:44 +0300

    update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject net.tbt-post/clj-kafka-x "0.8.0"
-  :description "A Clojure wrapper for Apache Kafka v2/v3 client"
+  :description "A Clojure wrapper for Apache Kafka"
   :url "https://github.com/source-c/clj-kafka-x"
   :license {:name "Apache License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
