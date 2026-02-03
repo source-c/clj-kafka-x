2024f79978a41cbddd93554ca2f3728c086b89f7
A I <io-devel@copper.mx>
2026-02-03 14:57:48 +0200

    update deps

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,9 @@
 {:dev      {:dependencies [[midje "1.10.10"]
-                           [org.slf4j/slf4j-simple "2.0.12"]
-                           [org.mockito/mockito-core "5.11.0"]]
+                           [org.slf4j/slf4j-simple "2.0.17"]
+                           [org.mockito/mockito-core "5.21.0"]]
             :test-paths   ["test"]
             :plugins      [[lein-cloverage "1.2.4"]]}
  :docs     {:plugins [[lein-codox "0.10.8"]
                       [org.timmc/nephila "0.3.0"]]}
- :provided {:dependencies [[org.clojure/clojure "1.12.0"]]}
+ :provided {:dependencies [[org.clojure/clojure "1.12.4"]]}
  :jar      {:aot :all}}
--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/source-c/clj-kafka-x"
   :license {:name "Apache License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.apache.kafka/kafka_2.12 "3.9.0"
+  :dependencies [[org.apache.kafka/kafka_2.12 "3.9.1"
                   :exclusions [com.sun.jdmk/jmxtools
                                com.sun.jmx/jmxri
                                org.slf4j/slf4j-log4j12]]
-                 [org.apache.kafka/kafka-clients "4.0.0"]])
+                 [org.apache.kafka/kafka-clients "4.1.1"]])
