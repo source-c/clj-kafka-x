c72f5aa6c88dc3ce5212a2127344a9a67c3e66b9
A I <io-devel@copper.mx>
2026-03-20 20:38:41 +0200

    update project - bump version, drop legacy kafka_2.12 scala wrapper, bump kafka-clients to 4.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,6 @@
-(defproject net.tbt-post/clj-kafka-x "0.8.1"
+(defproject net.tbt-post/clj-kafka-x "0.9.0"
   :description "A Clojure wrapper for Apache Kafka"
   :url "https://github.com/source-c/clj-kafka-x"
   :license {:name "Apache License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.apache.kafka/kafka_2.12 "3.9.1"
-                  :exclusions [com.sun.jdmk/jmxtools
-                               com.sun.jmx/jmxri
-                               org.slf4j/slf4j-log4j12]]
-                 [org.apache.kafka/kafka-clients "4.1.1"]])
+  :dependencies [[org.apache.kafka/kafka-clients "4.2.0"]])
